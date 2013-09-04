63ffce7c0ab36d1f7f9b8511dcf1e53feeb696ad
Matt Keller <m@cognusion.com>
2013-09-04 06:51:38 -0400

    Added views, and necessary fixes to get it working... And it does! First Post!

--- elasticchat.rb
+++ elasticchat.rb
@@ -8,10 +8,11 @@ require 'sinatra/cookies'
 require 'mongo'
 require 'json'
 require 'digest/sha2'
-require 'ap'
+require 'haml'
+require 'sanitize'
 require './spine'
 require './tokens'
-require 'sinatra/reloader'
+
 include Mongo # Import mongo symbols for convenience
 
 configure do
@@ -27,11 +28,30 @@ topics = Hash.new
 topics["test"] = MongoChat.new("test",db,key,iv) # Make sure we always have our base channel
   
 get '/' do
-  "<center>I feel sorry for people who don't drink.<br> 
-  When they wake up in the morning,<br> 
-  that's as good as they're going to feel all day.<br>
-  <br>
-  -- Frank Sinatra<br></center>"
+  haml :chatindex
+end
+
+post '/login/:topic' do |topic|
+  topics[topic] = MongoChat.new(topic,db,key,iv) unless topics.has_key?(topic)
+  
+  username = Sanitize.clean(params[:name], Sanitize::Config::RESTRICTED)
+  
+  topics[topic].add({:message => 'I have arrived!', :from => username, :style => 'announces to', :user => nil }, {:raw => true})
+
+  token = SecureToken.new(key,iv)
+  token.payload = username
+  cookies[:session] = token.to_e(true)
+    
+  haml :room, :locals => { :username => username, :topic => topic }
+    
+end
+
+get '/chat/:topic' do |topic|
+  topics[topic] = MongoChat.new(topic,db,key,iv) unless topics.has_key?(topic)
+  
+  username = topics[topic].user_from_request(request)
+  
+  haml :chattop, :locals => { :username => username, :topic => topic }
 end
 
 get '/subscribe/:topic/:epoch_stamp', :provides => 'text/html' do |topic,epoch|
@@ -53,9 +73,6 @@ end
 
 get '/subscribe/:topic', :provides => 'text/html' do |topic|
   topics[topic] = MongoChat.new(topic,db,key,iv) unless topics.has_key?(topic)
-  token = SecureToken.new(key,iv)
-  token.payload = "A USER NAME"
-  cookies[:session] = token.to_e(true)
     
   stream :keep_open do |out|
     EventMachine::PeriodicTimer.new(20) { out << topics[topic].HEARTBEAT }
@@ -65,30 +82,22 @@ get '/subscribe/:topic', :provides => 'text/html' do |topic|
 
 end
 
-post '/publish/:topic/:message' do
-#get '/publish/:topic/:message/:style/:touser' do |t,m,s,u|
+post '/publish/:topic' do |topic|
   # TODO: Hook in authentication requirement here
   
-  topic = Sanitize.clean(params[:topic], Sanitize::Config::RESTRICTED)
   message = Sanitize.clean(params[:message], Sanitize::Config::RELAXED)
   style = Sanitize.clean(params[:style], Sanitize::Config::RESTRICTED)
   toUser = Sanitize.clean(params[:to], Sanitize::Config::RESTRICTED)
-  toUser = nil if toUser == 'all'
-  #topic = t
-  #message = m
-  #style = s
-  #toUser = u
-  #toUser = nil if toUser == 'all' 
-  
+  toUser = nil if toUser == 'EVERYONE' or toUser == 'all'
    
   # Instantiate the room in this instance if needed
   topics[topic]  = MongoChat.new(topic,db,key,iv) unless topics.has_key?(topic)
 
-  stream do |out|
-    fromUser = topics[topic].user_from_connection(out)
-    topics[topic].add({:message => message, :from => fromUser, :style => style, :user => toUser }, {:raw => true})
-    out << "Sent"
-  end
+  username = topics[topic].user_from_request(request)
+  topics[topic].add({:message => message, :from => username, :style => style, :user => toUser }, {:raw => true})
+  
+  # Carry on
+  haml :chattop, :locals => { :username => username, :topic => topic }
   
 end
 
